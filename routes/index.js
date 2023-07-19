ad6c99b15ae612d027705c4cc7ea03dd88660787
gellisun <uengyaf@hotmail.it>
2023-07-19 17:05:14 +0400

    CSS final

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,9 @@ router.get('/', function(req, res, next) {
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
-  // Which passport strategy is being used?
   'google',
   {
-    // Requesting the user's profile and email
     scope: ['profile', 'email'],
-    // Optionally force pick account every time
-    // prompt: "select_account"
   }
 ));
 
